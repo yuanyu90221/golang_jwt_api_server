136b5701ea0e56779678e799edc6c7ac0bbc09de
jsonLiang <yuanyu90221@gmail.com>
2020-09-16 17:26:50 +0800

    finish the validator

--- api/models/User.go
+++ api/models/User.go
@@ -1,12 +1,73 @@
 package models
 
 import (
+	"errors"
 	"html"
 	"strings"
 	"time"
 
+	"github.com/badoux/checkmail"
+
 	"golang.org/x/crypto/bcrypt"
 )
+
+type validator interface {
+	validate(u *User) error
+}
+
+type DefaultAction struct {
+}
+
+func (a *DefaultAction) validate(u *User) error {
+	if u.Nickname == "" {
+		return errors.New("Required NickName")
+	}
+	if u.Passwd == "" {
+		return errors.New("Required Password")
+	}
+	if u.Email == "" {
+		return errors.New("Required Email")
+	}
+	if err := checkmail.ValidateFormat(u.Email); err != nil {
+		return errors.New("Invalid Email")
+	}
+	return nil
+}
+
+type updateAction struct {
+}
+
+func (a *updateAction) validate(u *User) error {
+	if u.Nickname == "" {
+		return errors.New("Required NickName")
+	}
+	if u.Passwd == "" {
+		return errors.New("Required Password")
+	}
+	if u.Email == "" {
+		return errors.New("Required Email")
+	}
+	if err := checkmail.ValidateFormat(u.Email); err != nil {
+		return errors.New("Invalid Email")
+	}
+	return nil
+}
+
+type loginAction struct {
+}
+
+func (a *loginAction) validate(u *User) error {
+	if u.Passwd == "" {
+		return errors.New("Required Password")
+	}
+	if u.Email == "" {
+		return errors.New("Required Email")
+	}
+	if err := checkmail.ValidateFormat(u.Email); err != nil {
+		return errors.New("Invalid Email")
+	}
+	return nil
+}
 
 type User struct {
 	ID        uint32    `gorm:"primary_key;auto_increment" json:"id"`
@@ -40,4 +101,8 @@ func (u *User) Prepare() {
 	u.Email = html.EscapeString(strings.TrimSpace(u.Email))
 	u.CreatedAt = time.Now()
 	u.UpdatedAt = time.Now()
+}
+
+func (u *User) Validate(v validator) error {
+	return v.validate(u)
 }
--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module github.com/yuanyu90221/golang_jwt_api_server
 go 1.14
 
 require (
-	github.com/badoux/checkmail v1.2.1 // indirect
+	github.com/badoux/checkmail v1.2.1
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
 	github.com/gorilla/mux v1.8.0 // indirect
 	github.com/jinzhu/gorm v1.9.16 // indirect
